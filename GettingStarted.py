a7569dd2340ace4ca05a1b2344744e2965a93df1
Rohit <rkumar8092378845@gmail.com>
2021-04-25 00:38:05 +0530

    Initial commit 25 Apr 12:35 A.M

---
+++ GettingStarted.py
@@ -0,0 +1,12 @@
+
+# Numpy introduction : https://www.w3schools.com/python/numpy/numpy_intro.asp
+
+import numpy as np
+
+print(np.__version__)
+
+# Creating array from numpy
+arr = np.array([1, 2, 3, 5])
+print(arr)
+
+
